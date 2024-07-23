328feb61f0540191ba13c484fcb3a9aa7a370526
Geronimo2763 <136124423+Geronimo2763@users.noreply.github.com>
2024-07-23 14:43:27 +0100

    Add files via upload

---
+++ codecademy_ext/content.js
@@ -0,0 +1,54 @@
+window.addEventListener('load', () => {
+    const url = window.location.href;
+    const targetUrl = 'https://www.codecademy.com/';
+
+    if (url.includes(targetUrl)) {
+        const interval = setInterval(() => {
+            if (document.querySelector('svg[aria-label="Loading next content"]')) {
+                clearInterval(interval); 
+                get_iframe();
+            } else {
+                console.log("loading");
+            }
+        }, 1000); // Check every second
+        
+
+
+        const button = document.createElement('button');
+        const myfooter = document.querySelector("footer");
+        let browser = document.querySelector('div[data-testid="web-browser"]');
+        let iframe;
+        const link = document.createElement("a");
+
+        function get_iframe() {
+            if (browser) {
+                iframe = browser.querySelector('iframe[title="Codecademy Web Browser"]');
+                if (iframe) {
+                    console.log(iframe);
+                    link.href = iframe.src;
+                    clearInterval(intervalId);
+                } else {
+                    console.log("Iframe not found");
+                }
+            }
+        }
+
+        const intervalId = setInterval(get_iframe, 1000);
+        
+        link.target = "_blank";
+
+        button.textContent = "Helloww";
+        link.className = "gamut-wddb2j-ResetElement-createButtonComponent"
+        button.style.zIndex = 100;
+        link.innerText = "Open In New Tab"
+        
+
+        
+
+
+
+        const footButtons = myfooter.firstChild.childNodes[2];
+
+        footButtons.insertBefore(link,footButtons.firstChild);
+    }
+})
